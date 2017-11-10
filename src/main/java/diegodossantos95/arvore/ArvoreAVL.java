4ae4afc98c664b550fef06d7482d58eea42235ab
dos Santos, Diego <diego.dos.santos@sap.com>
2017-11-10 18:43:44 -0200

    Finished adicionar method and getConteudo on ArvoreAVL class

--- src/main/java/diegodossantos95/arvore/ArvoreAVL.java
+++ src/main/java/diegodossantos95/arvore/ArvoreAVL.java
@@ -1,21 +1,22 @@
 package main.java.diegodossantos95.arvore;
 
+import java.text.Collator;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import main.java.diegodossantos95.tradutor.Dicionario;
 
 public class ArvoreAVL {
 	//TODO: Documentacao
-
 	private Dicionario raiz;
 	
 	public void adicionar(Dicionario dicionario){
 		if(raiz == null){
 			raiz = dicionario;
+		}else{
+			this.adicionar(this.raiz, dicionario);
 		}
-		
-		//TODO: adicionar quando nao for na raiz...
 	}
 	
 	public Dicionario procurarPelaPalavra(String palavra){
@@ -25,10 +26,58 @@ public class ArvoreAVL {
 	}
 	
 	public List<Dicionario> getConteudo(){
-		List<Dicionario> conteudo = new ArrayList<Dicionario>();
-		conteudo.add(raiz);
-		return conteudo;
-		
-		//TODO: percorrer arvore em ordem e retornar conteudo em List
+		List<Dicionario> lista = new ArrayList<Dicionario>();
+		this.percorrerEmOrdem(this.raiz, lista);
+		return lista;
+	}
+	
+	
+	/*
+	 * Metodos privados
+	 */
+	private void percorrerEmOrdem(Dicionario raiz, List<Dicionario> lista){
+		if(raiz == null){
+			return;
+		}
+		this.percorrerEmOrdem(raiz.getEsquerda(), lista);
+		lista.add(raiz);
+		this.percorrerEmOrdem(raiz.getDireita(), lista);
+	}
+	
+	private void adicionar(Dicionario raiz, Dicionario adicionar){
+		if(this.compararPalavras(adicionar.getPalavra(), raiz.getPalavra()) < 0){ //Esquerda
+			if(raiz.getEsquerda() == null){
+				raiz.setEsquerda(adicionar);
+				adicionar.setPai(raiz);
+				this.verificarBalanceamento(adicionar);
+			}else{
+				this.adicionar(raiz.getEsquerda(), adicionar);
+			}
+		}else if(this.compararPalavras(adicionar.getPalavra(), raiz.getPalavra()) > 0){ //Direita
+			if(raiz.getDireita() == null){
+				raiz.setDireita(adicionar);
+				adicionar.setPai(raiz);
+				this.verificarBalanceamento(adicionar);
+			}else{
+				this.adicionar(raiz.getDireita(), adicionar);
+			}
+		}
+	}
+	
+	private void verificarBalanceamento(Dicionario dicionario){
+		//TODO: algoritmo para verificar balanceamento
+	}
+	
+	private Integer compararPalavras(String comparador, String base){
+		/*
+		 * Compara as palavras ignorando acentos e case
+		 * Retorna 0 se for igual
+		 * Retorna -1 se o comparador for antes de base
+		 * Retorna 1 se o comparador for depois de base
+		 */
+		Collator collator = Collator.getInstance(Locale.US);
+        collator.setStrength(Collator.PRIMARY);
+        
+        return collator.compare(comparador, base);
 	}
 }
