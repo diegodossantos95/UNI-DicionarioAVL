92f87204e108a345942a7cf84e5dec21d16c4d9e
dos Santos, Diego <diego.dos.santos@sap.com>
2017-11-13 11:22:43 -0200

    Documentada a classe Tradutor

--- src/main/java/diegodossantos95/tradutor/Tradutor.java
+++ src/main/java/diegodossantos95/tradutor/Tradutor.java
@@ -12,15 +12,17 @@ import java.util.stream.Stream;
 import main.java.diegodossantos95.arvore.ArvoreAVL;
 
 public class Tradutor {
-
-	//TODO: Documentacao
+	/**
+	 * Referencia a arvore utilizada
+	 */
 	private ArvoreAVL arvore;
 	
+	/**
+	 * Metodo para carregar o arquivo do 
+	 * dicionario para a Arvore AVL
+	 * @param arquivo
+	 */
 	public void carregaDicionario(String arquivo) {
-		/*
-		 * Carrega o arquivo de dicionario 
-		 * (dicionario.dat) para a arvore AVL.
-		 */
 		try (Stream<String> stream = Files.lines(Paths.get(arquivo))) {
 			this.arvore = new ArvoreAVL(); 
 			stream.forEach(this::parseLinhaToDicionario);
@@ -29,43 +31,46 @@ public class Tradutor {
 		}
 	}
 	
+	/**
+	 * Metodo para traduzir uma unica palavra.
+	 * Recebe como parametro a palavra a ser traduzida
+	 * e retorna a lista das possiveis traducoes
+	 * para esta palavra, ou vazio caso a palavra
+	 * nao tenha sido encontrada
+	 * @param palavra
+	 * @return
+	 */
 	public LinkedList<String> traduzPalavra(String palavra) {
-		/*
-		 * Traduz uma unica palavra. Este metodo
-		 * recebe como parametro a palavra a ser
-		 * traduzida e retorna a lista das possiveis
-		 * traducoes para esta palavra.
-		 */
 		try{
 			Dicionario dicionario = this.arvore.procurarPelaPalavra(palavra);		
 			return dicionario.getDefinicoes(); 
 		}catch(Exception e){
-			//TODO: tratar errors
 			e.printStackTrace();
 			return new LinkedList<String>();
 		}
 	}
 
+	/**
+	 * Metodo para inserir uma nova definicao no dicionario.
+	 * Recebe como parametro a palavra em ingles
+	 * e a lista de possiveis traducoes
+	 * @param palavra
+	 * @param definicoes
+	 */
 	public void insereTraducao(String palavra, List<String> definicoes) {
-		/*
-		 * Insere uma nova definicao no dicionario.
-		 * Recebe como parametro a palavra em
-		 * ingles e lista de possiveis traducoes.
-		 */
 		try{
 			this.adicionarDicionario(palavra, definicoes);
 		}catch(Exception e){
-			//TODO: tratar errors
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * Metodo para salvar o arquivo do dicionario
+	 * com as respectivas definicoes baseado no conteudo da arvore AVL
+	 * @param arquivo
+	 */
 	public void salvaDicionario(String arquivo) {
-		/*
-		 * Salva o arquivo de dicionario
-		 * (dicionario.dat) com as respectivas
-		 * definicoes baseado no conteudo da arvore AVL
-		 */
 		try {
 			List<Dicionario> dicionarios = this.arvore.getConteudo();
 			String conteudo = dicionarios.stream()
@@ -73,7 +78,6 @@ public class Tradutor {
 				  .collect(Collectors.joining(System.lineSeparator()));
 			Files.write(Paths.get(arquivo), conteudo.getBytes());
 		} catch (IOException e) {
-			//TODO: tratar errors
 			e.printStackTrace();
 		}
 	}
@@ -81,16 +85,34 @@ public class Tradutor {
 	/*
 	 * Metodos privados
 	 */
+	/**
+	 * Metodo para parsear a linha do arquivo 
+	 * em instancias da classe Dicionario
+	 * @param linha
+	 */
 	private void parseLinhaToDicionario(String linha){
 		LinkedList<String> palavras = this.parseLinhaToList(linha);
 		this.adicionarDicionario(palavras.pop(), palavras);
 	}
 	
+	/**
+	 * Metodo para criar uma instancia de Dicionario 
+	 * e adicionar na Arvore AVL
+	 * @param palavra
+	 * @param definicoes
+	 */
 	private void adicionarDicionario(String palavra, List<String> definicoes){
 		Dicionario dicionario = new Dicionario(palavra, definicoes);
 		this.arvore.adicionar(dicionario);
 	}
 	
+	/**
+	 * Metodo para transformar uma String 
+	 * e uma linkedlist de String.
+	 * É utilizado o caracter # como separador na String.
+	 * @param linha
+	 * @return
+	 */
 	private LinkedList<String> parseLinhaToList(String linha){
 		LinkedList<String> palavras = new LinkedList<String>(Arrays.asList(linha.split("#")));
 		return palavras;
